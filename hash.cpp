b6c598dd67a12b5b478c6efe021968422ab90f88
Darren Fernandes <darrenmfdes@gmail.com>
2020-02-21 00:11:48 +0530

    first commit

---
+++ hash.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+class Book{
+    int id;
+    int score;
+
+    bool scanned = false;
+
+    Book(int a ,int b){
+        id = a;
+        score = b;
+    }
+};
+
+int main(){
+    
+    
+    return 0;
+}
